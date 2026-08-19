7d0eecc2ab0c37c340515dc030e5ec0e20362f7c
402-24-ramita <ramita16032554@gmail.com>
2026-08-19 15:14:45 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -33,6 +33,8 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
 
     u_ans1 = ans1.strip().lower()
     u_ans2 = ans2.strip().lower()
+    u_ans3 = ans3.strip().lower()
+    u_ans4 = ans4.strip().lower()
 
     # ตรวจข้อ 1
     if u_ans1 == "apple":
@@ -122,7 +124,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2)
+    show_result_dialog(ans1, ans2, ans3, ans4)
 
 st.divider()
 st.write("นางสาวรมิตา เทพนามวงค์ เลขที่ 24 ม.4/2")
